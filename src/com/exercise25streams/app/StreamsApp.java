18bd86c4f8c2553ef1b5117c3ee54e876e310496
DevUrielMC <urimarcervantes.95@gmail.com>
2019-10-24 11:43:13 -0500

    Ejercicio de clase stream

--- src/com/exercise25streams/app/StreamsApp.java
+++ src/com/exercise25streams/app/StreamsApp.java
@@ -1,10 +1,50 @@
 package com.exercise25streams.app;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 
-public class StreamsApp {
+public class StreamsApp 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		File myFile = null;
+		FileReader myReader= null;
+		BufferedReader myBuffer= null;
+		String line;
+		
+		
+		try {
+			myFile = new File("D:\\A\\hola.txt");
+			myReader= new FileReader (myFile);
+			myBuffer = new BufferedReader(myReader);
+			line=myBuffer.readLine();
+			//System.out.println(line);
+			
+			while((line= myBuffer.readLine())!=null)
+			{
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				myBuffer.close();
+				myReader=null;
+				myFile=null;
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
 	}
 
 }
